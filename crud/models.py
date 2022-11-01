e1db73db1d70590e979d7f9e226e66f3b9c15eb3
miltonmrq <miltonmarquez@iresm.edu.ar>
2022-11-01 11:27:13 -0300

    1/11/22

--- crud/models.py
+++ crud/models.py
@@ -1,13 +1,24 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.utils import timezone
 
 # Create your models here.
 
-class Actividad(models.Model):
-    descripcion = models.CharField(max_length=100)
-    encargado = models.CharField(max_length=100)
+class Tablero(models.Model):
+    encargado = models.CharField(max_length=255)
+    titulo = models.CharField(max_length=255, blank=False, null=False)
+    descripcion = models.CharField(max_length=255, blank=False, null=False)
+    creado = models.DateTimeField(default=timezone.now)
     estado = models.BooleanField(default=False)
     def __str__(self):
-        return "%s %s" % (self.descripcion, self.estado)
+        return f'{self.titulo} de {self.encargado}'
+
+class Lista(models.Model):
+    tablero = models.ForeignKey(Tablero, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=255, blank=False, null=False)
+    def __str__(self):
+        return {self.tablero}
+
 
 
 
